b67fec7fcafee5a590ee6a5b17795b41fd29a6a2
nur-e-jannatul-farjana <jannatulfarjana.ej@gmail.com>
2024-06-25 21:48:36 +0600

    even numbers

---
+++ even.js
@@ -0,0 +1,13 @@
+var num = 1;
+
+while(num < 10)
+    {
+        if (num%2==0)
+            {
+                console.log("Even - ",num);
+            }
+            else{
+                console.log("Odd - ",num);
+            }
+            num++;
+    }
