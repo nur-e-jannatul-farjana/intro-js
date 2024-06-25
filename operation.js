8f99bf9fa1a1d0e893e64465deb40f43865cd36f
nur-e-jannatul-farjana <jannatulfarjana.ej@gmail.com>
2024-06-25 11:38:29 +0600

    parse

--- operation.js
+++ operation.js
@@ -2,3 +2,7 @@ var first = 20;
 var sec = 50;
 console.log("Sum = ", first+sec);
 console.log("Difference = ", sec-first);
+
+var random = '6.3569';
+var random1= parseFloat(random);
+console.log(random1);
